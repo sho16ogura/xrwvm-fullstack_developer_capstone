c5e8e9fd81bef42b737dc7a57560a17ae0fe3cd9
sho16ogura <sho16_og@icloud.com>
2024-07-09 05:44:35 -0400

    added login logout functionalities

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
+import 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<LoginPanel />} />
     </Routes>
   );
 }
