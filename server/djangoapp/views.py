067a75791d6549330ab8197e3f7840633685d4d2
sho16ogura <sho16_og@icloud.com>
2024-07-11 13:45:39 -0400

    functionality to leave comment

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -109,9 +109,9 @@ def get_dealerships(request, state="All"):
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_details(request,dealer_id):
     if(dealer_id):
-        endpont = "/fetchDealer/"+str(dealer_id)
+        endpoint = "/fetchDealer/"+str(dealer_id)
         dealer = get_request(endpoint)
         return JsonResponse({"status":200, "dealer":dealer})
     else:
@@ -119,14 +119,14 @@ def get_dealer_reviews(request,dealer_id):
 
 
 # Create a `get_dealer_details` view to render the dealer details
-def get_dealer_details(request, dealer_id):
+def get_dealer_reviews(request, dealer_id):
     if(dealer_id):
-        endpont = "/fetchReviews/dealer/"+str(dealer_id)
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for r in reviews:
             resp = analyze_review_sentiments(r["review"])
             r["sentiment"] = resp["sentiment"]
-        return JsonResponse({"status":200, "dealer":dealer})
+        return JsonResponse({"status":200, "reviews":reviews})
     else:
         return JsonResponse({"status":400, "message": "Bad Request"})
 
