bf3ae350dad60457c8643112af8cba9190a0ee97
sho16ogura <sho16_og@icloud.com>
2024-07-12 03:29:18 -0400

    dockerized

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://ogurasho16-8000.\
-    theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://ogurasho16-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://ogurasho16-8000.\
-    theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://ogurasho16-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
